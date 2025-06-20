3571deb83c4acdccb8d9d490ad8fe726dcf55035
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-06-20 16:00:04 +0300

    home work

---
+++ homework/test_shop.py
@@ -0,0 +1,41 @@
+"""
+Протестируйте классы из модуля homework/models.py
+"""
+import pytest
+
+from homework.models import Product
+
+
+@pytest.fixture
+def product():
+    return Product("book", 100, "This is a book", 1000)
+
+
+class TestProducts:
+    """
+    Тестовый класс - это способ группировки ваших тестов по какой-то тематике
+    Например, текущий класс группирует тесты на класс Product
+    """
+
+    def test_product_check_quantity(self, product):
+        # TODO напишите проверки на метод check_quantity
+        assert product.check_quantity(0), f'Запрашиваемое количество книг не может быть отрицательным или превышает количеству книг на складе {product.quantity}'
+
+    def test_product_buy(self, product):
+        # TODO напишите проверки на метод buy
+        assert product.buy(11) != product.quantity, f'Запрашиваемое количество книг превышает количеству на складе'
+
+    def test_product_buy_more_than_available(self, product):
+        # TODO напишите проверки на метод buy,
+        #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
+        with pytest.raises(ValueError):
+            product.buy(product.quantity + 5)
+
+
+class TestCart:
+    """
+    TODO Напишите тесты на методы класса Cart
+        На каждый метод у вас должен получиться отдельный тест
+        На некоторые методы у вас может быть несколько тестов.
+        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
+    """
